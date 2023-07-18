9b7c35a2eb5614d730230202f3337bd189fb5bea
AymenMohamed-Ahmed <aymen.mo.ahmed1@gmail.com>
2023-07-18 03:35:27 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,15 @@
 #include "main.h"
-
+/**
+ * main - prints _putchar
+ * Return: 0
+ */
 int main(void)
 {
-    char str[] = "_putchar\n";
-    int i;
+	char str[] = "_putchar\n";
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-        _putchar(str[i]);
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
 
-    return (0);
+	return (0);
 }
