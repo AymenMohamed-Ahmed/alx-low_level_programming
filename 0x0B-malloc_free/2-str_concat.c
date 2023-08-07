368bf76cc1f3ce689ec3e57e8d0b57b8dd05b7a7
AymenMohamed-Ahmed <aymen.mo.ahmed1@gmail.com>
2023-08-07 07:44:04 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,32 +1,43 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* _strdup - returns a pointer to a newly allocated space in memory,
-* which contains a copy of the string given as a parameter
-* @str: the string to duplicate
+* str_concat - concatenates two strings
+* @s1: the first string
+* @s2: the second string
 *
-* Return: a pointer to the duplicated string, or NULL if str = NULL or
-* if insufficient memory was available
+* Return: pointer to the newly allocated memory containing the concatenated
+* string NULL on failure
 */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-char *dup;
-unsigned int i, len;
+char *concatenated;
+int len1 = 0, len2 = 0, i, j;
 
-if (str == NULL)
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
+while (s1[len1] != '\0')
+len1++;
+
+while (s2[len2] != '\0')
+len2++;
+
+concatenated = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+
+if (concatenated == NULL)
 return (NULL);
 
-for (len = 0; str[len]; len++)
-;
+for (i = 0; i < len1; i++)
+concatenated[i] = s1[i];
 
-dup = malloc((len + 1) * sizeof(char));
-if (dup == NULL)
-return (NULL);
+for (j = 0; j < len2; j++)
+concatenated[i + j] = s2[j];
 
-for (i = 0; i < len; i++)
-dup[i] = str[i];
-dup[len] = '\0';
+concatenated[i + j] = '\0';
 
-return (dup);
+return (concatenated);
 }
