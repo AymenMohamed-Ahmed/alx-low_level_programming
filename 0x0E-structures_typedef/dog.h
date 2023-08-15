29b03ac62488914e532b7ae924dbf0387905df85
AymenMohamed-Ahmed <aymen.mo.ahmed1@gmail.com>
2023-08-15 02:17:59 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,22 +17,5 @@ char *name;
 float age;
 char *owner;
 } dog_t;
-/**
- * main - Short description, single line
- *
- * Description: Longer description of the function
- *
- * Return: Description of the returned value
- */
 
-int main(void)
-{
-dog_t my_dog;
-
-my_dog.name = "Poppy";
-my_dog.age = 3.5;
-my_dog.owner = "Bob";
-printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-return (0);
-}
 #endif
