b1300f9c1288e556494500113c3b1b460e5add8b
AymenMohamed-Ahmed <aymen.mo.ahmed1@gmail.com>
2023-08-17 16:07:29 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -33,17 +33,17 @@ int main(int argc, char *argv[])
 if (argc != 2)
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 
 int num_bytes = atoi(argv[1]);
 if (num_bytes < 0)
 {
 printf("Error\n");
-return 2;
+return (2);
 }
 
 print_opcodes(main, num_bytes);
 
-return 0;
+return (0);
 }
