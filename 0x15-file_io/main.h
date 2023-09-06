26da6173cb422f48ecd1d33d2e710655fad19706
AymenMohamed-Ahmed <aymen.mo.ahmed1@gmail.com>
2023-09-06 06:07:45 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* LISTS_H */
